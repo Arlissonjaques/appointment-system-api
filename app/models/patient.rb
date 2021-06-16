b26a51078458a592744c1a9588643bab5fed1359
Arlisson Jaques <arlissonjaques@gmail.com>
2021-06-15 21:38:11 -0300

    feat(models): patient model creation

---
+++ app/models/patient.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: patients
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  birth_date :date
+#  cpf        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Patient < ApplicationRecord
+  has_many :appointments
+  has_many :doctors, through: :appointments
+
+  validates :name, :birth_date, :cpf, presence: true
+end
