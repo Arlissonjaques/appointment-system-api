b0737bbd1563afe68edd22d1d6a94a40a82ee797
Arlisson Jaques <arlissonjaques@gmail.com>
2021-06-18 17:33:23 -0300

    feat(dashboard): add filter in dashboard_controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,13 @@
 # controller responsible for appointment
 class DashboardController < ApplicationController
   def index
+    @doctors = Doctor.all
+    filter_by_query if params[:q]
+  end
 
+  private
+
+  def filter_by_query
+    @doctors = Doctor.ransack(name_or_crm_uf_cont: params[:q]).result
   end
 end
